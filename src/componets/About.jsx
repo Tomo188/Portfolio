78bf0ed21a0ad526837145e7bce59540266f0761
Tomo188 <tomislavkrtalic005@gmail.com>
2024-06-10 19:26:31 +0200

    fix

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -20,7 +20,7 @@ function About() {
   return <div  className="border-b border-neutral-900 pb-4">
   <h1 className="my-20 text-center text-4xl">About <span className="text-neutral-500">Me</span> </h1>
   <div className="flex flex-wrap">
-  <motion.div variants={animate(-100)} whileInView="initial" animate="visible" className="w-full lg:w-1/2 lg:pb-8">
+  <motion.div variants={animate(-100)} whileInView="visible" initial="inital" className="w-full lg:w-1/2 lg:pb-8">
     <div className="flex items-center justify-center">
          <img className="rounded-2xl" src={aboutImg} alt="About" />
     </div>
